cb5f11b9e577efcae7662caf5e5e8370bb8c741b
Your Name <jomerubaldo@gmail.com>
2025-08-11 01:14:08 +0800

    initial commit

--- function.js
+++ function.js
@@ -1,13 +1,61 @@
-const outPut = document.querySelector('#outPut');
-const input1 = document.querySelector('#input1');
-const input2 = document.querySelector('#input2');
-const submit = document.querySelector('#submit');
+// Get reference
+const displayOutPut = document.querySelector('#displayOutPut');
+const inputValue1 = document.querySelector('#inputValue1');
+const inputValue2 = document.querySelector('#inputValue2');
+const addButton = document.querySelector('#add');
+const minusButton = document.querySelector('#minus');
+const multiButton = document.querySelector('#multiplication');
+const divideButton = document.querySelector('#divide');
+const myResetButton = document.querySelector('#reset');
 
-submit.addEventListener('click', myFunction);
-function myFunction() {
-  const value1 = input1.value;
-  const value2 = input2.value;
-  const total = Number(value1) + Number(value2);
+// Create function Add
+addButton.addEventListener('click', myAddFunction);
+function myAddFunction() {
+  const addValue1 = inputValue1.value;
+  const addValue2 = inputValue2.value;
+  const total = Number(addValue1) + Number(addValue2);
 
-  document.querySelector('#outPut').innerHTML = total;
+  // Using DOM to display output
+  document.querySelector('#displayOutPut').innerHTML = total;
+}
+
+// Create function Add
+minusButton.addEventListener('click', myMinusFunction);
+function myMinusFunction() {
+  const minusValue1 = inputValue1.value;
+  const minusValue2 = inputValue2.value;
+  const total = Number(minusValue1) - Number(minusValue2);
+
+  // Using DOM to display output
+  document.querySelector('#displayOutPut').innerHTML = total;
+}
+
+// Create function multiplication
+multiButton.addEventListener('click', myMultiFunction);
+function myMultiFunction() {
+  const multiValue1 = inputValue1.value;
+  const multiValue2 = inputValue2.value;
+  const total = Number(multiValue1) * Number(multiValue2);
+
+  // Using DOM display output
+  document.querySelector('#displayOutPut').innerHTML = total;
+}
+
+// Create function divide
+divideButton.addEventListener('click', myDivideFunction);
+function myDivideFunction() {
+  const divideValue1 = inputValue1.value;
+  const divideValue2 = inputValue2.value;
+  const total = Number(divideValue1) / Number(divideValue2);
+
+  // Using DOM display output
+  document.querySelector('#displayOutPut').innerHTML = total;
+}
+
+// Need to fix it to reset the value
+myResetButton.addEventListener('click', myResetFunction);
+function myResetFunction() {
+  displayOutPut;
+
+  document.querySelector('#displayOutPut').innerHTML = reset;
 }
