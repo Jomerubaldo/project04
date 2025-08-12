1b2e82e1d070713ac87149d99256e6d0a11afdcc
Your Name <jomerubaldo@gmail.com>
2025-08-12 15:09:14 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # project04
+
+## Calculator App
+
+### Do basic math with buttons and display results with simple conditional if the user enter no value show alert message
--- function.js
+++ function.js
@@ -16,14 +16,22 @@ addButton.addEventListener('click', myAddFunction);
 function myAddFunction() {
   const addValue1 = inputValue1.value;
   const addValue2 = inputValue2.value;
+
+  // Check if the user enter a value if not display message
+  if (inputValue1.value === '' || inputValue2.value === '') {
+    alert('Please Enter a Value!');
+    return;
+  }
+
+  // Store value and do the function here
   const total = Number(addValue1) + Number(addValue2);
+
+  // Using DOM to display output
+  document.querySelector('#displayOutPut').innerHTML = total;
 
   // Clear input Every click function
   inputValue1.value = '';
   inputValue2.value = '';
-
-  // Using DOM to display output
-  document.querySelector('#displayOutPut').innerHTML = total;
 }
 
 // Create function Add
@@ -31,14 +39,22 @@ minusButton.addEventListener('click', myMinusFunction);
 function myMinusFunction() {
   const minusValue1 = inputValue1.value;
   const minusValue2 = inputValue2.value;
+
+  // Check if the user enter a value if not display message
+  if (inputValue1.value === '' || inputValue2.value === '') {
+    alert('Please re enter the value!');
+    return;
+  }
+
+  // Store value and do the function here
   const total = Number(minusValue1) - Number(minusValue2);
+
+  // Using DOM to display output
+  document.querySelector('#displayOutPut').innerHTML = total;
 
   // Clear input Every click function
   inputValue1.value = '';
   inputValue2.value = '';
-
-  // Using DOM to display output
-  document.querySelector('#displayOutPut').innerHTML = total;
 }
 
 // Create function multiplication
@@ -46,14 +62,22 @@ multiButton.addEventListener('click', myMultiFunction);
 function myMultiFunction() {
   const multiValue1 = inputValue1.value;
   const multiValue2 = inputValue2.value;
+
+  // Check if the user enter a value if not display message
+  if (inputValue1.value === '' || inputValue2.value === '') {
+    alert('Please re enter the value!');
+    return;
+  }
+
+  // Store value and do the function here
   const total = Number(multiValue1) * Number(multiValue2);
+
+  // Using DOM display output
+  document.querySelector('#displayOutPut').innerHTML = total;
 
   // Clear input Every click function
   inputValue1.value = '';
   inputValue2.value = '';
-
-  // Using DOM display output
-  document.querySelector('#displayOutPut').innerHTML = total;
 }
 
 // Create function divide
@@ -61,14 +85,22 @@ divideButton.addEventListener('click', myDivideFunction);
 function myDivideFunction() {
   const divideValue1 = inputValue1.value;
   const divideValue2 = inputValue2.value;
+
+  // Check if the user enter a value if not display message
+  if (inputValue1.value === '' || inputValue2.value === '') {
+    alert('Please re enter the value!');
+    return;
+  }
+
+  // Store value and do the function here
   const total = Number(divideValue1) / Number(divideValue2);
+
+  // Using DOM display output
+  document.querySelector('#displayOutPut').innerHTML = total;
 
   // Clear input Every click function
   inputValue1.value = '';
   inputValue2.value = '';
-
-  // Using DOM display output
-  document.querySelector('#displayOutPut').innerHTML = total;
 }
 
 // Create function reset
